f23b82d1d0053d02be64c5ccf775d9b0c0bb3288
Enio Lotero <elotero@baufest.com>
2022-08-17 14:54:19 -0300

    Now using portals to display backdrop & overlays

--- src/components/ModalInfo/ModalInfo.js
+++ src/components/ModalInfo/ModalInfo.js
@@ -1,23 +1,44 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Button from "../Button/Button";
 
 import './ModalInfo.css';
 
+function Backdrop(props) {
+    return <div className="backdrop" onClick={props.onClose}/>;
+}
+
+function Overlay(props) {
+    return (
+        <div className="modal-info">
+            <header>
+                {props.title}
+            </header>
+            <div className="message">
+                {props.message}
+            </div>
+            <footer>
+                <Button onClick={props.onClose} label='Ok'/>
+            </footer>
+        </div>
+    );
+}
+
 function ModalInfo(props) {
     return (
         <>
-            <div className="backdrop" onClick={props.onClose}/>
-            <div className="modal-info">
-                <header>
-                    {props.title}
-                </header>
-                <div className="message">
-                    {props.message}
-                </div>
-                <footer>
-                    <Button onClick={props.onClose} label='Ok'/>
-                </footer>
-            </div>
+            {
+                ReactDOM.createPortal(
+                    <Backdrop onClose={props.onClose}/>,
+                    document.getElementById("backdrop-root")
+                )
+            }
+            {
+                ReactDOM.createPortal(
+                    <Overlay title={props.title} message={props.message} onClose={props.onClose} />,
+                    document.getElementById("overlay-root")
+                )
+            }
         </>
     );
 }
